ea8ccccf0ee52dbf8667ebbd086441b5bc20147e
DhanushM12 <dhanushmukhi@gmail.com>
2023-02-08 19:03:03 +0530

    Added base code and timer

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import '../App.css'
+
+function App() {
+  let time = new Date().toLocaleTimeString();
+  // const [stateName, setterMethodforState] = useState(initialValue)
+  const [currentTime, setCurrentTime] = useState(time);
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time)
+  }
+  setInterval(updateTime, 1000)
+  return (
+    <div className='App'>
+      <h1>{currentTime}</h1>
+    </div>
+  )
+}
+
+export default App
